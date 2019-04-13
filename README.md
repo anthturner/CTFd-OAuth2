9e83ea9c4e88085fd3197e5e96754a1b17a7f842
Anthony Turner <anthony@bitoblivion.com>
2019-04-13 13:14:33 -0700

    Fix Azure authentication plugin, add GitHub support

--- README.md
+++ README.md
@@ -6,15 +6,16 @@ Add OAuth2 authentication to CTFd 2.x using compatible providers. Users can be l
 
 ## Supported Providers:
 * `azure` (Azure Active Directory)
+* `github` (GitHub)
 * ... and any other Flask-Dance provider, with a little work!
 
 ## Configuration
 The following configuration options must be defined in `config.py`:
 ```
-OAUTH_CLIENT_ID - OAuth2 Provider Client ID
-OAUTH_CLIENT_SECRET - OAuth2 Provider Client Secret
-OAUTH_PROVIDER - OAuth2 Provider name (see 'Supported Providers' above)
-OAUTH_CREATE_MISSING_USER - If the plugin should create a CTFd user to link to the OAuth2 user
+OAUTHLOGIN_CLIENT_ID - OAuth2 Provider Client ID
+OAUTHLOGIN_CLIENT_SECRET - OAuth2 Provider Client Secret
+OAUTHLOGIN_PROVIDER - OAuth2 Provider name (see 'Supported Providers' above)
+OAUTHLOGIN_CREATE_MISSING_USER - If the plugin should create a CTFd user to link to the OAuth2 user
 ```
 
 ## Extensibility:
@@ -24,21 +25,19 @@ Adding a new provider is as simple as adding entries to the two lambda dictionar
 # Key is the OAuth provider name
 # Value is a lambda that returns a Blueprint for the authentication controller
 provider_blueprints = {
-    'azure': lambda: return flask_dance.contrib.make_azure_blueprint(
-        login_url='/auth/azure',
+    'azure': lambda: flask_dance.contrib.azure.make_azure_blueprint(
+        login_url='/azure',
         client_id=oauth_client_id,
         client_secret=oauth_client_secret,
-        redirect_url="/auth/azure/confirm")
+        redirect_url=authentication_url_prefix + "/azure/confirm")
 }
 
 # Key is the OAuth provider name
 # Value is a lambda that returns a User object mapped from the OAuth user, or None if the user doesn't exist and creation is disabled.
 provider_users = {
     'azure': lambda: 
-        resp = flask_dance.contrib.azure.get("/v1.0/me").json()
-        return get_bridge_user(
-            displayName=resp["displayName"],
-            email=resp["userPrincipalName"]
-        )
+        get_bridge_user(
+            displayName=flask_dance.contrib.azure.azure.get("/v1.0/me").json()["displayName"],
+            email=flask_dance.contrib.azure.azure.get("/v1.0/me").json()["userPrincipalName"])
 }
 ```
--- oauth2.py
+++ oauth2.py
@@ -1,74 +1,107 @@
-from flask import render_template, session
+from flask import render_template, session, redirect
+from flask_dance.contrib import azure, github
 import flask_dance.contrib
 
 from CTFd.auth import confirm, register, reset_password, login
 from CTFd.models import db, Users
+from CTFd.utils import set_config
 from CTFd.utils.logging import log
 from CTFd.utils.security.auth import login_user, logout_user
 
 from CTFd import utils
 
 def load(app):
-    oauth_client_id = utils.get_app_config('OAUTH_CLIENT_ID')
-    oauth_client_secret = utils.get_app_config('OAUTH_CLIENT_SECRET')
-    oauth_provider = utils.get_app_config('OAUTH_PROVIDER')
-    create_missing_user = utils.get_app_config('OAUTH_CREATE_MISSING_USER')
+    ########################
+    # Plugin Configuration #
+    ########################
+    authentication_url_prefix = "/auth"
+    oauth_client_id = utils.get_app_config('OAUTHLOGIN_CLIENT_ID')
+    oauth_client_secret = utils.get_app_config('OAUTHLOGIN_CLIENT_SECRET')
+    oauth_provider = utils.get_app_config('OAUTHLOGIN_PROVIDER')
+    create_missing_user = utils.get_app_config('OAUTHLOGIN_CREATE_MISSING_USER')
 
-    # Either retrieve the user (if exists), create it (if appropriate), or return None
-    def get_bridge_user(displayName, email):
-        user = Users.query.filter_by(email=resp['userPrincipalName']).first()
+    ##################
+    # User Functions #
+    ##################
+    def retrieve_user_from_database(username):
+        user = Users.query.filter_by(email=username).first()
         if user is not None:
-            log('logins', "[{date}] {ip} - {name} OAuth2 bridged user found")
+            log('logins', "[{date}] {ip} - " + user.name + " - OAuth2 bridged user found")
             return user
-        if not create_missing_user:
-            log('logins', "[{date}] {ip} - {name} No OAuth2 bridged user found and not configured to create missing users")
-            return None
+    def create_user(username, displayName):
         with app.app_context():
-            log('logins', "[{date}] {ip} - {name} No OAuth2 bridged user found, creating user")
-            user = new Users(
-                name=displayName.strip(),
-                email=email.lower(),
-                password="abc123".strip()
-            )
+            log('logins', "[{date}] {ip} - " + user.name + " - No OAuth2 bridged user found, creating user")
+            user = Users(email=username, name=displayName.strip())
             db.session.add(user)
             db.session.commit()
             db.session.flush()
             return user
+    def create_or_get_user(username, displayName):
+        user = retrieve_user_from_database(username)
+        if user is not None:
+            return user
+        if create_missing_user:
+            return create_user(username, displayName)
+        else:
+            log('logins', "[{date}] {ip} - " + user.name + " - No OAuth2 bridged user found and not configured to create missing users")
+            return None
 
-    # Lambdas to create blueprints for the authentication endpoints
+    ##########################
+    # Provider Configuration #
+    ##########################
     provider_blueprints = {
-        'azure': lambda: return flask_dance.contrib.make_azure_blueprint(
-            login_url='/auth/azure',
+        'azure': lambda: flask_dance.contrib.azure.make_azure_blueprint(
+            login_url='/azure',
             client_id=oauth_client_id,
             client_secret=oauth_client_secret,
-            redirect_url="/auth/azure/confirm")
+            redirect_url=authentication_url_prefix + "/azure/confirm"),
+        'github': lambda: flask_dance.contrib.github.make_github_blueprint(
+            login_url='/github',
+            client_id=oauth_client_id,
+            client_secret=oauth_client_secret,
+            redirect_url=authentication_url_prefix + "/github/confirm")
     }
 
-    # Lambdas to retrieve/create users
+    def get_azure_user():
+        user_info = flask_dance.contrib.azure.azure.get("/v1.0/me").json()
+        return create_or_get_user(
+            username=user_info["userPrincipalName"],
+            displayName=user_info["displayName"])
+    def get_github_user():
+        user_info = flask_dance.contrib.github.github.get("/user").json()
+        return create_or_get_user(
+            username=user_info["email"],
+            displayName=user_info["name"])
+
     provider_users = {
-        'azure': lambda: 
-            resp = flask_dance.contrib.azure.get("/v1.0/me").json()
-            return get_bridge_user(
-                displayName=resp["displayName"],
-                email=resp["userPrincipalName"]
-            )
+        'azure': lambda: get_azure_user(),
+        'github': lambda: get_github_user()
     }
 
-    provider_blueprint = provider_blueprints[oauth_provider]()
+    provider_blueprint = provider_blueprints[oauth_provider]() # Resolved lambda
+    
+    #######################
+    # Blueprint Functions #
+    #######################
+    @provider_blueprint.route('/<string:auth_provider>/confirm', methods=['GET'])
+    def confirm_auth_provider(auth_provider):
+        if not provider_users.has_key(auth_provider):
+            return redirect('/')
 
-    @provider_blueprint.route('/auth/<string:auth_provider>/confirm', methods=['GET'])
-    def confirm_auth_provider(req_auth_provider):
+        provider_user = provider_users[oauth_provider]() # Resolved lambda
         session.regenerate()
-        user = provider_users[req_auth_provider]()
-        if user is not None:
-            login_user(user)
-        return redirect_url('/')
+        if provider_user is not None:
+            login_user(provider_user)
+        return redirect('/')
 
-    app.register_blueprint(provider_blueprints[oauth_provider])
+    app.register_blueprint(provider_blueprint, url_prefix=authentication_url_prefix)
 
-    # TODO: Disable backend (POST) as well
+    ###############################
+    # Application Reconfiguration #
+    ###############################
     # ('', 204) is "No Content" code
-    app.view_functions['auth.login'] = lambda: ('', 204)
+    set_config('registration_visibility', False)
+    app.view_functions['auth.login'] = lambda: redirect(authentication_url_prefix + "/" + oauth_provider)
     app.view_functions['auth.register'] = lambda: ('', 204)
     app.view_functions['auth.reset_password'] = lambda: ('', 204)
     app.view_functions['auth.confirm'] = lambda: ('', 204)     
